a813c12baf8486b2192cce14aee1232dc7b17909
naveen p <44580258+kill5Witchd@users.noreply.github.com>
2022-04-12 23:26:58 +0530

    Add files via upload

---
+++ april12/react-app/src/App.js
@@ -0,0 +1,15 @@
+
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+  
+function App() {
+    return ( 
+      
+      <div className='App'>
+      <h1> Hello World! </h1>
+      </div>
+      
+    );
+}
+  
+export default App;
